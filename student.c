8834a8740fdee7bf56b2e2bb7e1a926d634a674f
rajvichaudhari07 <69470226+rajvichaudhari07@users.noreply.github.com>
2020-09-28 17:44:40 +0530

    Create student.c

---
+++ student.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+struct StudentData{
+    char *stu_name;
+    int stu_id;
+    int stu_age;
+};
+int main()
+{
+     struct StudentData student;
+     student.stu_name = "Steve";
+     student.stu_id = 1234;
+     student.stu_age = 30;
+     printf("Student Name is: %s", student.stu_name);
+     printf("\nStudent Id is: %d", student.stu_id);
+     printf("\nStudent Age is: %d", student.stu_age);
+     return 0;
+}
